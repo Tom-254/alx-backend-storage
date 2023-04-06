636abde4ce20b0980aac0c32b850d130ee78188c
Tom-254 <211mseeratiego@gmail.com>
2023-04-06 21:56:45 +0000

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (COALESCE(split, '2020') - formed) AS lifespan
 FROM metal_bands
 WHERE CONCAT(',', COALESCE(style, ''), ',') LIKE '%,Glam rock,%'
