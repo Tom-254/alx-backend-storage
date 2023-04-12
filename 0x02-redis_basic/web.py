1e154c275027323d0b5f6ae1e699b8cb3bee3a34
Tom-254 <211mseeratiego@gmail.com>
2023-04-12 19:00:44 +0000

    0x02. Redis basic

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Allows Caching to Redis
+"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
+    """Caches the output of fetched data.
+    """
+    @wraps(method)
+    def invoker(url) -> str:
+        """The wrapper function for caching the output.
+        """
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """Caches the response from a request to
+    the URL passed and returns
+    """
+    return requests.get(url).text
